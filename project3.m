9dec6b1900e6597c5bae34709e4821da3f376952
ibrahim güngen <gungenibrahim@gmail.com>
2018-05-26 23:38:15 +0300

    matlab üzerinden analitik dizayn yapılmaya başlandı
    Amaç hem raporu hem dizaynı matlab üzerinden hazırlamak. Traction motor tasarımı seçildi.

---
+++ project3.m
@@ -0,0 +1,66 @@
+%% EE564 DESIGN OF ELECTRICAL MACHINES
+% PROJECT 3 - TRACTION MOTOR DESIGN
+
+
+%% INTRODUCTION
+% In this project, traction asynchronous squirrel cage induction motor 
+%(with copper rotor-bars) is designed. Then, designed motor will be analyzed 
+% with FEM program. Specificitions of the generator as follows,
+
+% * Rated Power Output: 1280 kW
+% * Line-to-line voltage: 1350 V
+% * Number of poles: 6
+% * Rated Speed: 1520 rpm (72 km/h) (driven with 78 Hz inverter)
+% * Rated Motor Torque: 7843 Nm
+% * Cooling: Forced Air Cooling
+% * Insulating Class: 200C
+% * Train Wheel Diameter: 1210 mm
+% * Maximum Speed: 140 km/h
+% * Gear Ratio: 4.82
+
+% Design steps at below will be followed at this design.
+
+% # Starting values
+% # Main dimensions D and l'
+% # Air Gap
+% # Winding Selection Qs, Qr and kw
+% # Air-gap flux density Bs
+% #
+% #
+%%
+
+%% Starting Values
+
+Prated = 1280; %W
+Vl_l = 1350; %V
+Npole = 6;
+n_rated = 1520; %rpm
+T_rated = 7843; %Nm
+f_rated = 78; %Hz
+v_rated = 74; %km/h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
